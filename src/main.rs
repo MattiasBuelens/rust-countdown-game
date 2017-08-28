b29d3559e1a00afb64680234fdeb15498e1c889e
Mattias Buelens <mattias@buelens.com>
2017-08-28 23:12:48 +0200

    Tweak CLI.

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,19 @@ use solve::{solve, SolveStats};
 
 fn main() {
     let matches: ArgMatches = clap_app!(myapp =>
+        (name: "countdown-game")
         (version: "0.1.0")
         (author: "Mattias Buelens <mattias.buelens@gmail.com>")
         (about: "Solves the Numbers round from the Countdown game show")
         (@arg TARGET: -t --target +required +takes_value "Target number")
-        (@arg NUMBERS: +required +takes_value +multiple "Numbers to use")
+        (@arg NUMBER: +required +multiple "Numbers to use")
     ).get_matches();
 
     let target_str = matches.value_of("TARGET").unwrap();
     let target = target_str.parse::<i32>().unwrap();
 
     let mut numbers: Vec<i32> = Vec::new();
-    for number_str in matches.values_of("NUMBERS").unwrap() {
+    for number_str in matches.values_of("NUMBER").unwrap() {
         numbers.push(number_str.parse::<i32>().unwrap());
     }
 
